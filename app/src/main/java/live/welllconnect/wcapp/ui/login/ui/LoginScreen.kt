5ebb83ccf753ee365992e0e191f196a1157ba577
cookyourweb <hello.cookyourweb@gmail.com>
2023-07-26 17:19:36 +0200

    Added android proyect

---
+++ app/src/main/java/live/welllconnect/wcapp/ui/login/ui/LoginScreen.kt
@@ -0,0 +1,8 @@
+package live.welllconnect.wcapp.ui.login.ui
+
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun LoginScreen (){
+}
---
+++ app/src/main/java/live/welllconnect/wcapp/ui/login/ui/LoginViewModel.kt
@@ -0,0 +1,2 @@
+package live.welllconnect.wcapp.ui.login.ui
+
